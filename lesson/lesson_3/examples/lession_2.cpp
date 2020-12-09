7e3c653ce6042802eefdef16c108802a68a923c4
zhengtq <1553866519@qq.com>
2020-12-09 16:31:42 +0800

    modify lesson typo

---
+++ lesson/lesson_3/examples/lession_2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include "blob.h"
+#include "datareader.h"
+#include "layer.h"
+#include "net.h"
+using namespace std;
+
+int main(int argc, char** argv) {
+    ncnn::Net net;
+    ncnn::Layer layer;
+    ncnn::Blob blob;
+    printf("yes\n");
+    return 0;
+}
---
+++ lesson/lesson_3/src/layer.cpp
@@ -0,0 +1,11 @@
+#include "layer.h"
+#include <string.h>
+#include <iostream>
+using namespace std;
+
+namespace ncnn {
+
+Layer::Layer() { cout << "Layer has been constructed" << endl; }
+
+Layer::~Layer() {}
+}
